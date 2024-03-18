9552eeaa4f7579a757b7f1b487c181f3bfe68d59
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-18 21:09:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 26765615
 api_hash = "dc9335059a80b3c8753d26e36d9e28f4"
-bot_token = "7183436145:AAGbNdiMTVi4NUnwJfMLpjvkPbMwyrsEjto"
+bot_token = "6998818558:AAF1jHqX345vVgeoHo3NOcq-zmmaylebEew"
 AUTH = 6389088397
-#Bot Username = "Jeiehdvdgevdbot"
+#Bot Username = "Choudhary9360_bot"

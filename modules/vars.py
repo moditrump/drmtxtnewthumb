49af1fe12cc1e0d09db1f12ad0095c77215a0b20
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-20 16:30:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 20491966
 api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
-bot_token = "7009426514:AAFJ3C6QYZSGIRBHq3MSFnUbGE5PHMeLKyc"
+bot_token = "7187632152:AAFVczTif72lY4hDIDjbaNpK2jqoFBuWE2I"
 AUTH = 7168441486
 #Bot Username = "hackedevbot"
